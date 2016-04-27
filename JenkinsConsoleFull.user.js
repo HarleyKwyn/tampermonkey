54040f87525445bfffd49176492960f4758bdd3d
Kwyn Meagher <Kwyn.Meagher@neustar.biz>
2016-04-27 09:42:41 -0700

    Fix @include tag for jenkins full console script

--- JenkinsConsoleFull.user.js
+++ JenkinsConsoleFull.user.js
@@ -4,32 +4,32 @@
 // @version      1.0.1
 // @description  Add a button to full console output for Jenkins
 // @author       Kwyn Meagher
-// @include      https://neustar.ci.cloudbees.com/job/*
+// @include      https://**neustar**/job/*
 // @grant        none
-// @downloadURL  https://github.com/AK-Scripts/tampermonkey/raw/master/jenkinsConsoleFull.user.js
+// @downloadURL  https://github.com/AK-Scripts/tampermonkey/raw/master/JenkinsConsoleFull.user.js
 // ==/UserScript==
 jQuery(function() {
   var consoleTask, newTask;
   var tasks = jQuery('#tasks')[0].children;
-  
+
   for (var i = 0; i < tasks.length; i++) {
     var task = tasks[i];
     var title = task.innerHTML.toLowerCase();
-    
+
     if(title.indexOf('console output') > -1){
       consoleTask = jQuery(task);
       newTask = consoleTask.clone();
       var newTaskLinks = newTask.find('a');
-      
+
       for (var j = 0; j < newTaskLinks.length; j++) {
-        var link = newTaskLinks[j]
+        var link = newTaskLinks[j];
         var oldHref = link.href;
-        var newHref = oldHref+'Full'
-        if(link.className === 'task-link'){
+        var newHref = oldHref+'Full';
+        if (link.className === 'task-link') {
           jQuery(link).text('Full Console Output');
         }
         newTaskLinks[j].href = newHref;
-      };  
+      };
     }
   };
   consoleTask.after(newTask);
